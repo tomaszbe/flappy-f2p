2b5e7cbd963ddf6c5de077bc1d34a82f9e5496b3
Tomasz Barański <tomaszbe@hotmail.com>
2017-10-06 16:07:21 +0200

    Play sound on collision.
    - Added Audio Source to Player object
    - Created a script to play the sound on collision

---
+++ Assets/AudioPlayer.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioPlayer : MonoBehaviour {
+
+	public AudioSource collisionAudioSource;
+
+	void Start() {
+		if (collisionAudioSource == null) {
+			collisionAudioSource = GetComponent<AudioSource> ();
+		}
+	}
+
+	void OnCollisionEnter(Collision collision) {
+		collisionAudioSource.Play ();
+	}
+
+}
