96a36f4285baadddc99e14747e94794b0b6e2e21
Tomasz Barański <tomaszbe@hotmail.com>
2017-10-06 16:14:17 +0200

    Added comments to AudioPlayer.cs

--- Assets/AudioPlayer.cs
+++ Assets/AudioPlayer.cs
@@ -7,12 +7,14 @@ public class AudioPlayer : MonoBehaviour {
 	public AudioSource collisionAudioSource;
 
 	void Start() {
+		// If no AudioSource assigned, let's get it ourselves.
 		if (collisionAudioSource == null) {
 			collisionAudioSource = GetComponent<AudioSource> ();
 		}
 	}
 
 	void OnCollisionEnter(Collision collision) {
+		// Play the sound on collision.
 		collisionAudioSource.Play ();
 	}
 
